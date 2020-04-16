62b9342ac6fa9b3b24e4a6952ba72b79fef1736a
phozzy <arthur.fayzullin@gmail.com>
2020-04-16 15:56:47 +0200

    Create test cluster

--- main.tf
+++ main.tf
@@ -1,8 +1,30 @@
-variable "test_key" {
+variable "cloud_key" {
     type = string
 }
-output "test" {
-    value = "${var.test_key}"
-}
 resource "null_resource" "test" {
+}
+provider "google" {
+  credentials = "${var.cloud_key}"
+  project     = "samuel-test-00"
+  region      = "us-central1"
+  version     = "~> 3.12.0"
+}
+resource "google_container_cluster" "primary" {
+  name     = "dojo-gke-cluster"
+  location = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "dojo-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
 }
