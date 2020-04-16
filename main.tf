9e5f530cdcf4ef789a3150026ab302c4f4417a05
phozzy <arthur.fayzullin@gmail.com>
2020-04-16 16:16:56 +0200

    Use autoscaling node group

--- main.tf
+++ main.tf
@@ -19,7 +19,10 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "dojo-node-pool"
   location   = "us-central1"
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  autoscaling {
+      min_node_count = 0
+      max_node_count = 3
+  }
   node_config {
     preemptible  = true
     machine_type = "n1-standard-1"
