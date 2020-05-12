4564fd7e6c3decbf244170b62a6b2ea9874d6da1
phozzy <arthur.fayzullin@gmail.com>
2020-05-12 14:52:43 +0200

    Format code accourding to guidelines

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 variable "cloud_key" {
-    type = string
+  type = string
 }
 resource "null_resource" "test" {
 }
 provider "google" {
-  credentials = "${var.cloud_key}"
+  credentials = var.cloud_key
   project     = "samuel-test-00"
   region      = "us-central1"
   version     = "~> 3.12.0"
 }
 resource "google_container_cluster" "primary" {
-  name     = "dojo-gke-cluster"
-  location = "us-central1"
+  name                     = "dojo-gke-cluster"
+  location                 = "us-central1"
   remove_default_node_pool = true
   initial_node_count       = 1
 }
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "dojo-node-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.primary.name
+  name     = "dojo-node-pool"
+  location = "us-central1"
+  cluster  = google_container_cluster.primary.name
   autoscaling {
-      min_node_count = 0
-      max_node_count = 3
+    min_node_count = 0
+    max_node_count = 3
   }
   node_config {
     preemptible  = true
