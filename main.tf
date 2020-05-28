b36c093c8ff029f3d48fc7d1dbe0b72857a117fd
phozzy <arthur.fayzullin@gmail.com>
2020-05-28 13:07:32 +0000

    Fix release channel option

--- main.tf
+++ main.tf
@@ -15,7 +15,9 @@ resource "google_container_cluster" "primary" {
   location                 = "us-central1-a"
   remove_default_node_pool = true
   initial_node_count       = 1
-  release_channel          = "RAPID"
+  release_channel {
+    channel = "RAPID"
+  }
 }
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name     = "dojo-node-pool"
